42883ba69042b5587e8effe9a0bfe06581ba5021
oosok4 <oosok4@naver.com>
2018-05-15 10:30:24 +0900

    2018-05-15 puppy #1

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
 <template>
   <section class="section">
-    <div>
-    <h1 class="title">퍼피</h1>
-    <h2 class="subtitle">개사랑</h2>
+    <div class="hero is-danger">
+      <div clss="hero-body">
+       <div class="container">
+          <h1 class="title is-1">퍼피</h1>
+          <h2 class="subtitle">개사랑</h2>
+       </div>
+      </div>
     </div>
   </section>
 </template>
