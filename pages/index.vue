bd51ecd326a357d17ea83fe7c8d2086b53df48f7
oosok4 <oosok4@naver.com>
2018-05-15 10:53:09 +0900

    2018-05-15 puppy #3

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,6 @@
     <br>
     <article class="message">
       <div class="message-header">개란 무엇인가?</div>
-      <div class="message-body">afdgdfg</div>
+      <div class="message-body">강아지를 구분하는 척도는 연령이므로 원래라면 크기에 관계없이 어린 개만을 강아지라고 부를 수 있다. 그러나 요즘에는 성견이라 할지라도 자신이 키우는 소형견을 강아지로 부르는 일도 자주있다.</div></article>
   </section>
 </template>
