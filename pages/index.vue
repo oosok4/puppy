5b452b96dd24b0de9561ba65fc52dd44ec8eeed3
oosok4 <oosok4@naver.com>
2018-05-15 10:45:09 +0900

    2018-05-15 puppy #2

--- pages/index.vue
+++ pages/index.vue
@@ -8,5 +8,10 @@
        </div>
       </div>
     </div>
+    <br>
+    <article class="message">
+      <div class="message-header">개란 무엇인가?</div>
+      <div class="message-body">강아지는 개의 어린 형태를 일컫는 순우리말이다. 소의 새끼 → 송아지, 말 → 망아지의 어법을 따라 만들어진 용어다. 돌, 돝 + 아지 → 도야지 → 돼지도 있다. 영어로는 Puppy 혹은 Doggy[1]라고 한다.<div>
+      </br>
   </section>
 </template>
